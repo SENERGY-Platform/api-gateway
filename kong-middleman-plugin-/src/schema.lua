f50ddd97119adcb23d1b03c2c1f07a59ce7459a7
franzmueller <franzmueller@users.noreply.github.com>
2019-05-15 07:59:31 +0200

    remove outdated files

--- kong-middleman-plugin-/src/schema.lua
+++
@@ -1,9 +0,0 @@
-return {
-  no_consumer = true,
-  fields = {
-    url = {required = true, type = "string"},
-    response = { required = true, default = "table", type = "string", enum = {"table", "string"}},
-    timeout = { default = 10000, type = "number" },
-    keepalive = { default = 60000, type = "number" }
-  }
-}
